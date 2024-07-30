9907fbf44a25bbe3902033e9283ba67493663ef2
Tanish Bandari <tanishbandari@gmail.com>
2024-07-30 12:41:00 +0530

    first and second task

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,13 +13,20 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  if (book) {
+    res.send(book);
+  } 
+  else {
+    res.send("ISBN not found.");
+  }
  });
   
 // Get book details based on author
