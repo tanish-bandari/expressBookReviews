bfb942d3dda681883adfadca78d056c9590b5ba7
Tanish Bandari <tanishbandari@gmail.com>
2024-07-30 15:43:39 +0530

    up to task 5 complete

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,7 +7,21 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log(username, password);
+  if (username && password) {
+    if (!isValid(username.trim())) {
+      users.push({username:username.trim(), password:password.trim()});
+      return res.status(200).json({ message: "Registration complete." });
+    } 
+    else {
+      return res.status(404).json({ message: "Username taken." });
+    }
+  }
+  else{
+    return res.status(404).json({ message: "Registration failed." });
+  }
 });
 
 // Get the book list available in the shop
@@ -20,7 +34,7 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
-  const book = books[isbn];
+  const book = books[isbn.trim()];
   if (book) {
     res.send(book);
   } 
@@ -49,13 +63,29 @@ public_users.get('/author/:author',function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const bookList = Object.values(books);
+
+  const titleFilter = bookList.filter(book => book.title.trim() == title.trim());
+  if (titleFilter.length > 0) {
+    res.send(titleFilter);
+  } 
+  else {
+    res.send("Title not found.");
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn.trim()];
+  if (book){
+    res.send(book.reviews);
+  }
+  else{
+    res.send("ISBN not found.")
+  }
 });
 
 module.exports.general = public_users;
