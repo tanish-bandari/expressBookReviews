3e1dd437a8ab2f17bfd326f77e8b104e4d996d71
Tanish Bandari <tanishbandari@gmail.com>
2024-07-31 14:48:46 +0530

    project complete

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,16 +23,16 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', async function (req, res) {
     //Write your code here
     res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
     //Write your code here
     const isbn = req.params.isbn;
-    const book = books[isbn.trim()];
+    const book = await books[isbn.trim()];
     if (book) {
         res.send(book);
     } 
@@ -42,13 +42,13 @@ public_users.get('/isbn/:isbn',function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
     //Write your code here
     const author = req.params.author;
-    const bookList = Object.values(books);
+    const bookList = await Object.values(books);
     // console.log(bookList);
 
-    const authorFilter = bookList.filter(book => book.author.trim() == author.trim());
+    const authorFilter = await bookList.filter(book => book.author.trim() == author.trim());
     // console.log(authorFilter);
     if (authorFilter.length > 0) {
         res.send(authorFilter);
@@ -59,12 +59,12 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
     //Write your code here
     const title = req.params.title;
-    const bookList = Object.values(books);
+    const bookList = await Object.values(books);
 
-    const titleFilter = bookList.filter(book => book.title.trim() == title.trim());
+    const titleFilter = await bookList.filter(book => book.title.trim() == title.trim());
     if (titleFilter.length > 0) {
         res.send(titleFilter);
     } 
