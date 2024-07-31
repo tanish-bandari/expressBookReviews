90f342c30074098a1d3a5ee60ed53fb09cba4487
Tanish Bandari <tanishbandari@gmail.com>
2024-07-31 13:13:57 +0530

    new adding and deleting

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -56,26 +56,30 @@ regd_users.post("/login", (req,res) => {
 regd_users.put("/auth/review/:isbn", (req, res) => {
     //Write your code here
     const isbn = req.params.isbn;
-    const review = req.query.review;
-    const user = req.session.authenticated.username;
-    let bookReviews = books[isbn.trim()].reviews;
+    const review = req.body.review;
+    const username = req.session.authorization.username;
+    if (books[isbn.trim()]) {
+        let book = books[isbn.trim()];
+        book.reviews[username] = review;
+        return res.status(200).send("Review successfully posted");
+    }
+    else {
+        return res.status(404).json({message: `ISBN ${isbn} not found`});
+    }
+});
 
-    if (!bookReviews){
-        res.send("ISBN ${isbn} does not exist.")
-    }
-
-    let reviewExists = false;
-    for (const username in bookReviews) {
-        if (username == user) {
-            bookReviews[user] = review;
-            reviewExists = true;
-            break;
-        }
-    }
-    if (!reviewExists) {
-        bookReviews[user] = review;
-    }
-    return res.json({message: "The review for ISBN ${isbn} has been added."});
+// delete book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.authorization.username;
+  if (books[isbn]) {
+      let book = books[isbn];
+      delete book.reviews[username];
+      return res.status(200).send("Review successfully deleted");
+  }
+  else {
+      return res.status(404).json({message: `ISBN ${isbn} not found`});
+  }
 });
 
 module.exports.authenticated = regd_users;
